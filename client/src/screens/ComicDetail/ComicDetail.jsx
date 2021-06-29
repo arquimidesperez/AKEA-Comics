4e2bfccff0c22e390107b34889cda1b15e3826d3
bhonestly <ernestbizzell@gmail.com>
2021-06-29 22:45:35 +0300

    comicdetail and comicedit functions built

--- client/src/screens/ComicDetail/ComicDetail.jsx
+++ client/src/screens/ComicDetail/ComicDetail.jsx
@@ -1,8 +1,27 @@
 import React from 'react'
+import { useEffect, useState }  from 'react'
 import { Layout } from '../../components'
+import { getComic, deleteComic } from '../../services/comics'
+import { useParams, Link } from 'react-router-dom'
 
-//deleted props  
-export default function ComicDetail() {
+export default function ComicDetail(props) {
+  const [comic, setComic] = useState(null)
+  const [isLoaded, setLoaded] = useState(false)
+  const { id } = useParams()
+
+  useEffect(() => {
+    const fetchComic = async () => {
+      const comic = await getComic(id)
+      setComic(comic)
+      setLoaded(true)
+    }
+    fetchComic()
+  }, [id])
+
+  if (!isLoaded) {
+    return <h1>Loading...</h1>
+  }
+
   return (
     // remvoed until needed from layout user={props.user}
     <Layout >
