f08bf26d1c3387657f4c00299dcaa9f9450922b4
Kevin Wand <kevinwand@Kevins-Air.home>
2021-06-29 15:58:27 -0400

    changes to comicbrowse, comiccards, and comics

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,48 +1,48 @@
-import './App.css';
-import { useState, useEffect } from 'react'
-import Home from './screens/Home/Home'
-import ComicBrowse from './screens/ComicBrowse/ComicBrowse'
-import ComicCreate from './screens/ComicCreate/ComicCreate'
-import ComicEdit from './screens/ComicEdit/ComicEdit'
-import ComicDetail from './screens/ComicDetail/ComicDetail'
-import { Route, Switch, Redirect } from 'react-router-dom'
-import { verifyUser } from './services/users'
-import SignUp from './screens/SignUp/SignUp'
-import Login from './screens/Login/Login'
-import Logout from './screens/Logout/Logout'
+import "./App.css";
+import { useState, useEffect } from "react";
+import Home from "./screens/Home/Home";
+import ComicBrowse from "./screens/ComicBrowse/ComicBrowse";
+import ComicCreate from "./screens/ComicCreate/ComicCreate";
+import ComicEdit from "./screens/ComicEdit/ComicEdit";
+import ComicDetail from "./screens/ComicDetail/ComicDetail";
+import { Route, Switch, Redirect } from "react-router-dom";
+import { verifyUser } from "./services/users";
+import SignUp from "./screens/SignUp/SignUp";
+import Login from "./screens/Login/Login";
+import Logout from "./screens/Logout/Logout";
 
 const App = () => {
-  const [user, setUser] = useState(null)
+  const [user, setUser] = useState(null);
 
   useEffect(() => {
     const fetchUser = async () => {
-      const user = await verifyUser()
-      user ? setUser(user) : setUser(null)
-    }
-    fetchUser()
-  }, [])
+      const user = await verifyUser();
+      user ? setUser(user) : setUser(null);
+    };
+    fetchUser();
+  }, []);
 
   return (
     <div className="App">
       <Switch>
         <Route exact path="/">
-          <Home/>
+          <Home />
         </Route>
         <Route exact path="/create-comic">
-          <ComicCreate/>
+          <ComicCreate />
         </Route>
         <Route exact path="/comic-details">
-          <ComicDetail/>
+          <ComicDetail />
         </Route>
         <Route exact path="/edit-comic">
-          <ComicEdit/>
+          <ComicEdit />
         </Route>
-        <Route exact path="/browse">
-          <ComicBrowse/>
+        <Route exact path="/comics">
+          <ComicBrowse />
         </Route>
       </Switch>
     </div>
   );
-}
+};
 
 export default App;
--- client/src/components/ComicCards/ComicCards.jsx
+++ client/src/components/ComicCards/ComicCards.jsx
@@ -1,9 +1,15 @@
-import React from 'react'
+import React from "react";
+import "./ComicCards.css";
+import { Link } from "react-router-dom";
 
-export default function ComicCards() {
+export default function ComicCards(props) {
   return (
-    <div>
-      hello ComicCards
-    </div>
-  )
+    <>
+      <Link className="comic" to={`/comics/${props._id}`}>
+        <img className="comic-image" src={props.imgURL} alt={props.title} />
+        <div className="comic-title">{props.title}</div>
+        <div className="comic-brand">{props.brand}</div>
+      </Link>
+    </>
+  );
 }
