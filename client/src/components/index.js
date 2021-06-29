5e77bb270fce58d4ee77e3a0283b2901661f87f0
bhonestly <ernestbizzell@gmail.com>
2021-06-29 17:20:11 +0300

    builtout frontend files data rendering to page

---
+++ client/src/components/index.js
@@ -0,0 +1,7 @@
+import Layout from './Layout/Layout'
+import Comic from './Comic/Comic'
+// import Search from './Search/Search'
+// import Sort from './Sort/Sort'
+import ComicCards from './ComicCards/ComicCards'
+
+export { Layout, Comic, ComicCards }  // add in search, sort when/if we decide to use
