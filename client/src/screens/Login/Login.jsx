a87363f44de086023b8830ec833fe0aae7a7db25
Arquimides <arquimides@Arquimidess-MacBook-Air.local>
2021-06-30 11:31:09 -0400

    began building out authentication

---
+++ client/src/screens/Login/Login.jsx
@@ -0,0 +1,84 @@
+import { useState } from 'react'
+import './Login.css'
+import { signIn } from '../../services/users'
+import { useHistory } from 'react-router-dom'
+
+const Login = (props) => {
+  const history = useHistory()
+
+  const [form, setForm] = useState({
+    email: '',
+    password: '',
+    isError: false,
+    errorMsg: '',
+  })
+
+  const handleChange = (event) => {
+    setForm({
+      ...form,
+      [event.target.name]: event.target.value,
+    })
+  }
+
+  const onLogin = async (event) => {
+    event.preventDefault()
+    const { setUser } = props
+    try {
+      const user = await signIn(form)
+      setUser(user)
+      history.push('/comics')
+    } catch (error) {
+      console.error(error)
+      setForm({
+        isError: true,
+        errorMsg: 'Invalid Credentials',
+        email: '',
+        password: '',
+      })
+    }
+  }
+
+  const renderError = () => {
+    const toggleForm = form.isError ? 'Invalid Login' : ''
+    if (form.isError) {
+      return (
+        <button type='submit' className={toggleForm}>
+          {form.errorMsg}
+        </button>
+      )
+    } else {
+      return <button type='submit'>Login</button>
+    }
+  }
+
+  const { email, password } = form
+
+  return (
+    <div className='form-container'>
+      <h3>Log In</h3>
+      <form onSubmit={onLogin}>
+        <label>Email</label>
+        <input
+          required
+          type='text'
+          name='email'
+          value={email}
+          placeholder='Enter Email'
+          onChange={handleChange}
+        />
+        <label>Password</label>
+        <input
+          required
+          name='password'
+          value={password}
+          type='password'
+          placeholder='Password'
+          onChange={handleChange}
+        />
+        {renderError()}
+      </form>
+    </div>
+  )
+}
+
+export default Login
