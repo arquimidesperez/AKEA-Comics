25570899724580b3f0590cde0dec68e118a8c481
Arquimides <arquimides@Arquimidess-MacBook-Air.local>
2021-06-30 20:54:02 -0400

    began CSS

--- client/src/components/Nav/Nav.jsx
+++ client/src/components/Nav/Nav.jsx
@@ -5,19 +5,30 @@ import { Logout } from "../../screens/Logout/Logout";
 // eslint-disable-next-line
 {/* <NavLink to="/edit-comic">Edit</NavLink> */ }
 
+/**
+ * Want the Welcome, User to be the same size; slight issue where it appears smallers
+ */
 
 const Nav = ({ user }) => {
 
   return (
     <nav>
-      <div className="nav">
-        <NavLink to="/comics">Home</NavLink>
-        <br />
-        {user ? (<NavLink className="create" to="/create-comic">Create</NavLink>) : (<NavLink className="create-greyed-out" to="/signup">Create</NavLink>)}
-        <br />
-        {user ? `Welcome, ${user.username}` : <NavLink to="/signup">Sign Up Now</NavLink>}
-        <br />
-        {user ? <NavLink to="/logout">Logout</NavLink> : <NavLink to="/login">Login</NavLink>}
+      <div className="nav-div">
+      <div className="nav-spacing">
+        <NavLink className='nav-link logo' to="/">AKEA COMICS</NavLink>
+        </div>
+        <div className="nav-spacing">
+        <NavLink className='nav-link' to="/comics">Browse</NavLink>
+        </div>
+        <div className="nav-spacing">
+        {user ? (<NavLink className="nav-link nav-create" to="/create-comic">Create</NavLink>) : (<NavLink className="nav-link create-greyed-out" to="/signup">Create</NavLink>)}
+        </div>
+        <div className="nav-spacing">
+        {user ? `Welcome, ${user.username}` : <NavLink className='nav-link' to="/signup">Sign Up Now</NavLink>}
+        </div>
+        <div className="nav-spacing">
+        {user ? <NavLink className='nav-link' to="/logout">Logout</NavLink> : <NavLink className='nav-link' to="/login">Login</NavLink>}
+        </div>
       </div>
     </nav>
   );
