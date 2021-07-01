05e520abd06093af25c59298961726666724a352
Kevin Wand <kevinwand@Kevins-Air.home>
2021-07-01 10:48:57 -0400

    landing page css v1.0

--- client/src/screens/Home/Home.jsx
+++ client/src/screens/Home/Home.jsx
@@ -10,10 +10,10 @@ export default function Home() {
           WELCOME TO AKEA COMICS
         </h3>
         <p className="landing-body">
-          Where the DC and Marvel universes collide.  Buy, sell, or browse all your favorites here!
+          <strong>Where the DC and Marvel universes collide.  Buy, sell, or browse all your favorites here!</strong>
         </p>
         <button className="landing-button">
-          <Link to="/comics">Browse All Comics</Link>
+          <Link className="landing-link-text" to="/comics">Browse All Comics</Link>
         </button>
       </div>
     </div>
