6aa5f0a7f64416476f644768f9fa58b920738829
Angela Maruca <angelamaruca@Angelas-MacBook-Pro.local>
2021-06-30 23:41:39 -0400

    implemented edit and delete functionality, including reoutes, restructioned create comic form

--- client/src/screens/ComicDetail/ComicDetail.jsx
+++ client/src/screens/ComicDetail/ComicDetail.jsx
@@ -1,53 +1,59 @@
-import { useEffect, useState } from 'react'
-import './ComicDetail.css'
-import { Layout } from '../../components'
+import { useEffect, useState } from "react";
+import "./ComicDetail.css";
+import { Layout } from "../../components";
 // eslint-disable-next-line
-import { getComic, deleteComic } from '../../services/comics'
+import { getComic, deleteComic } from "../../services/comics";
 // eslint-disable-next-line
-import { useParams, Link } from 'react-router-dom'
+import { useParams, Link } from "react-router-dom";
 
 export default function ComicDetail(props) {
   // eslint-disable-next-line
-  const [comic, setComic] = useState(null)
-  const [isLoaded, setLoaded] = useState(false)
-  const { id } = useParams()
+  const [comic, setComic] = useState(null);
+  const [isLoaded, setLoaded] = useState(false);
+  const { id } = useParams();
 
   useEffect(() => {
     const fetchComic = async () => {
-      const comic = await getComic(id)
-      setComic(comic)
-      setLoaded(true)
-    }
-    fetchComic()
-  }, [id])
+      const comic = await getComic(id);
+      setComic(comic);
+      setLoaded(true);
+    };
+    fetchComic();
+  }, [id]);
 
   if (!isLoaded) {
-    return <h1>Loading...</h1>
+    return <h1>Loading...</h1>;
   }
-console.log(comic.id)
+  console.log(comic.id);
   return (
     <Layout user={props.user}>
-      <div className='comic-details'>
-        <div className='comic-container'>
-          <div className='comicbook-cover'>
+      <div className="comic-details">
+        <div className="comic-container">
+          <div className="comicbook-cover">
             <img
-              className='comicbook-art'
+              className="comicbook-art"
               src={comic.imgURL}
               alt={comic.title}
             />
           </div>
-          <div className='comic-information'>
-            <div className='title'>{comic.title}</div>
-            <div className='description'>{comic.description}</div>
-            <div className='price'>{comic.price}</div>
+          <div className="comic-information">
+            <div className="title">{comic.title}</div>
+            <div className="description">{comic.description}</div>
+            <div className="price">{comic.price}</div>
           </div>
         </div>
-        <Link to={`/edit-comic/${id}`}><button>Edit</button></Link>
-        <button>Delete</button>
-        
-      
+        <Link to={`/edit-comic/${id}`}>
+          <button>Edit</button>
+        </Link>
+        <Link to="/comics">
+          <button
+            className="delete-button"
+            onClick={() => deleteComic(comic._id)}
+          >
+            Delete
+          </button>
+        </Link>
       </div>
     </Layout>
-   
-  )
+  );
 }
