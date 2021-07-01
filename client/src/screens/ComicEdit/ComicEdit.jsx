971b8a7c0b378e29bdb847a40917157faa7a2631
Arquimides <arquimides@Arquimidess-MacBook-Air.local>
2021-07-01 11:00:30 -0400

    kevin almost nuked me

--- client/src/screens/ComicEdit/ComicEdit.jsx
+++ client/src/screens/ComicEdit/ComicEdit.jsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react";
+import './ComicEdit.css'
 import { Layout } from "../../components";
 import { getComic, updateComic } from "../../services/comics";
 import { useParams, Redirect } from "react-router-dom";
@@ -43,69 +44,72 @@ export default function ComicEdit(props) {
   return (
     <Layout user={props.user}>
       <div className="comic-edit">
-        <div className="image-container">
-          <img
-            className="edit-product-image"
-            src={comic.imgURL}
-            alt={comic.name}
-          />
-          <form onSubmit={handleSubmit}>
+        <div className='editor-container'>
+          <h3 className='editor-text'>Update</h3>
+            <div className="image-container">
+              <img
+                className="edit-product-image"
+                src={comic.imgURL}
+                alt={comic.name}
+              />
+            <form onSubmit={handleSubmit}>
             <input
-              className="input-image-link"
-              placeholder="Image Link"
-              value={comic.imgURL}
-              name="imgURL"
-              required
-              onChange={handleChange}
-            />
+                className="input-name"
+                placeholder="title"
+                value={comic.title}
+                name="name"
+                required
+                autoFocus
+                onChange={handleChange}
+              />
+              </form>
+            </div>
+            <form className="edit-form" onSubmit={handleSubmit}>
+            <input
+                  className="input-image-link"
+                  placeholder="Image Link"
+                  value={comic.imgURL}
+                  name="imgURL"
+                  required
+                  onChange={handleChange}
+                />
+              <br />
+              <input
+                className="input-price"
+                placeholder="Price"
+                value={comic.price}
+                name="price"
+                required
+                onChange={handleChange}
+              />
+              <br />
+              <input
+                className="input-brand"
+                placeholder="DC or Marvel"
+                value={comic.brand}
+                name="brand"
+                required
+                onChange={handleChange}
+              />
+              <div className='editor-text'>
+              Description:
+              </div>
+              <textarea
+                className="textarea-description"
+                rows={10}
+                cols={78}
+                placeholder="Description"
+                value={comic.description}
+                name="description"
+                required
+                onChange={handleChange}
+              />
+              <br />
+              <button type="submit" className="save-button">
+                Save Changes
+              </button>
           </form>
         </div>
-        <form className="edit-form" onSubmit={handleSubmit}>
-          <input
-            className="input-name"
-            placeholder="title"
-            value={comic.title}
-            name="name"
-            required
-            autoFocus
-            onChange={handleChange}
-          />
-          <br />
-          <input
-            className="input-price"
-            placeholder="Price"
-            value={comic.price}
-            name="price"
-            required
-            onChange={handleChange}
-          />
-          <br />
-          <input
-            className="input-brand"
-            placeholder="DC or Marvel"
-            value={comic.brand}
-            name="brand"
-            required
-            onChange={handleChange}
-          />
-          <br />
-          Description:
-          <br />
-          <textarea
-            className="textarea-description"
-            rows={10}
-            cols={78}
-            placeholder="Description"
-            value={comic.description}
-            name="description"
-            required
-            onChange={handleChange}
-          />
-          <br />
-          <button type="submit" className="save-button">
-            Save Changes
-          </button>
-        </form>
       </div>
     </Layout>
   );
