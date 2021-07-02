3f3a796b6fb4582a2d810f87b3770dbdb69e2c48
Angela Maruca <angelamaruca@Angelas-MacBook-Pro.local>
2021-07-02 16:55:36 -0400

    silenced warnings for deployment

--- client/src/components/Carousel/Carousel.jsx
+++ client/src/components/Carousel/Carousel.jsx
@@ -1,11 +1,11 @@
-import { useState }from 'react'
+// import { useState }from 'react'
 import { CarouselData } from './CarouselData'
 import {FaArrowAltCircleRight, FaArrowAltCircleLeft} from "react-icons/fa"
 
 
 
 export default function Carousel() {
-  const [current, setCurrent] = useState(0)
+  // const [current, setCurrent] = useState(0)
   // const length = items.length
 
 
@@ -17,7 +17,7 @@ export default function Carousel() {
       
       {CarouselData.map((comic, index) => {
         return (
-          <img src={comic.image} alt="image not available"/>
+          <img key={index} src={comic.image} alt="not available"/>
         )
       }
       )}
