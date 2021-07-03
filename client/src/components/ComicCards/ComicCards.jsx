08a85af25d44d774be5e6dd1724f8fe83f672b2f
Arquimides <arquimides@Arquimidess-MacBook-Air.local>
2021-07-03 12:46:10 -0400

    comicCards & browse

--- client/src/components/ComicCards/ComicCards.jsx
+++ client/src/components/ComicCards/ComicCards.jsx
@@ -9,13 +9,10 @@ export default function ComicCards(props) {
         <Link className="comic" to={`/comics/${props._id}`}>
 
           <div className='comic-cards-individuals'>
-            {/* <div className='comic-image-container'> */}
-            <img className="comic-image" src={props.imgURL} alt={props.title} />
-            {/* </div> */}
-
+          <img className="comic-image" src={props.imgURL} alt={props.title} />
             <div className='comic-info-container'>
               <div className="comic-title">{props.title}</div>
-              {/* <div className="comic-brand">{props.brand}</div> */}
+              <div className="comic-brand">{props.brand}</div>
 
             </div>
 
