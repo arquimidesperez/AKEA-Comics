2785ccf6f3201d69905055e458e021faeda06756
Arquimides <arquimides@Arquimidess-MacBook-Air.local>
2021-07-06 19:29:11 -0400

    footer with linkedin

--- client/src/components/Footer/Footer.jsx
+++ client/src/components/Footer/Footer.jsx
@@ -6,6 +6,23 @@ export default function Footer() {
       <div className='copyright-info'>
         AKEA COMICS
       </div>
+
+      <div className="footer-link-holder">
+        <div className='footer-names'>
+        <a href='https://www.linkedin.com/in/angela-maruca/' target='blank' className='footer-a'>Angela</a>
+        </div>
+        <div className='footer-names'>
+        <a href='https://www.linkedin.com/in/arquimides-perez-leyva/' target='blank' className='footer-a'>Arquimides</a>
+        </div>
+        <div className='footer-names'>
+        <a href='https://www.linkedin.com/in/ernestbizzell/' target='blank' className='footer-a'>Ernest</a>
+        </div>
+        <div className='footer-names'>
+        <a href='https://www.linkedin.com/in/kevin-wand/' target='blank' className='footer-a'>Kevin</a>
+        </div>
+
+      </div>
     </div>
   )
 }
+
