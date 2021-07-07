12a4dc77370cccd85e4dedefbb070993f51bd68a
Kevin Wand <kevinwand@Kevins-Air.home>
2021-07-07 10:23:07 -0400

    updated wishlist

--- client/src/screens/Wishlist/Wishlist.jsx
+++ client/src/screens/Wishlist/Wishlist.jsx
@@ -4,40 +4,46 @@ import { useState, useEffect } from "react";
 import { getUserWishlist } from "../../services/users";
 import "./Wishlist.css";
 import { useParams } from 'react-router-dom';
-import DeleteWishlist from '../../components/DeleteWishlist/DeleteWishlist';
+import WishlistCard from '../../components/WishlistCard/WishlistCard';
 
 export default function Wishlist() {
   const { id } = useParams()
-  const [comics, setComics] = useState();
-
-  const fetchComics = async () => {
-    const allComics = await getUserWishlist(id);
-    setComics(allComics.wishlist);
-  };
+  const [wishlist, setWishlist] = useState();
 
   useEffect(() => {
-    fetchComics();
+    const fetchWishlist = async () => {
+      const user = await getUserWishlist(id);
+      setWishlist(user.wishlist);
+    };
+
+    fetchWishlist();
   }, []);
 
+  const removeFromWishlist = (comicId) => {
+    const updatedWishlist = wishlist.filter((comic) => comic._id !== comicId)
+    setWishlist(updatedWishlist)
+  }
 
   return (
     <>
       <Layout >
-        <div className="wishlist-card">
-          {comics && comics.map((comic, index) => {
-            return (
-              <DeleteWishlist
-                _id={comic._id}
-                title={comic.title}
-                imgURL={comic.imgURL}
-                price={comic.price}
-                brand={comic.brand}
-                description={comic.description}
-                fetchComics={fetchComics}
-                key={index}
-              />
-            );
-          })}
+        <div className='wishlist-background'>
+          <div className="wishlist-card">
+            {wishlist && wishlist.map((comic, index) => {
+              return (
+                <WishlistCard
+                  _id={comic._id}
+                  title={comic.title}
+                  imgURL={comic.imgURL}
+                  price={comic.price}
+                  brand={comic.brand}
+                  description={comic.description}
+                  removeFromWishlist={removeFromWishlist}
+                  key={index}
+                />
+              );
+            })}
+          </div>
         </div>
       </Layout>
     </>
